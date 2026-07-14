c7441aa5e8c8626b28f7b666b6163fd807880bfe
Hiran-Joy <hiranjoy7@gmail.com>
2026-07-15 02:42:01 +0530

    FInally done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import NavigationBar from "./components/NavigationBar";
 import AddEv from "./components/AddEv";
@@ -9,8 +9,12 @@ function App() {
   return (
     <BrowserRouter>
       <NavigationBar />
-      <AddEv />
-      <ViewEv />
+
+      <Routes>
+        <Route path="/" element={<AddEv />} />
+        <Route path="/add" element={<AddEv />} />
+        <Route path="/view" element={<ViewEv />} />
+      </Routes>
     </BrowserRouter>
   );
 }
