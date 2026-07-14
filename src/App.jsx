c4699917ddab757f380d21e6de890e2c2067abb9
Hiran-Joy <hiranjoy7@gmail.com>
2026-07-15 02:22:42 +0530

    Nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import AddEv from './components/AddEv'
-import ViewEv from './components/ViewEv'
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+import NavigationBar from "./components/NavigationBar";
+import AddEv from "./components/AddEv";
+import ViewEv from "./components/ViewEv";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <AddEv/>
-      <ViewEv/>
-    </>
-  )
+    <BrowserRouter>
+      <NavigationBar />
+      <AddEv />
+      <ViewEv />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
